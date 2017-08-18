5d66726932b586bb13e3cc4b52d331b80354737a
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-18 21:18:27 +0900

    방_시간 선택

---
+++ app/controllers/time_room_controller.rb
@@ -0,0 +1,4 @@
+class TimeRoomController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'time_room/index'
+
   get 'time_school/index'
 
   devise_for :users
