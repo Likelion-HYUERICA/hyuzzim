e3bf433d51eb81a705d30c235df1e8d4be873510
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-08-18 20:20:13 +0900

    학교 디비 시드 추가

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+School.create([
+                  {school_name: '공학대학'},
+                  {school_name: '소프트웨어융합대학'},
+                  {school_name: '디자인대학'},
+                  {school_name: '경상대학'},
+                  {school_name: '국제문화대학'},
+                  {school_name: '예체능대학'},
+                  {school_name: '언론정보대학'}
+              ])
